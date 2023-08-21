cb9e2c0eee831bc870743a4c605527a5c4ca8e0b
nobutaka <n.takushima@gmail.com>
2023-08-22 03:24:47 +0900

    WIP entry point

--- minigfx.c
+++ minigfx.c
@@ -5,13 +5,8 @@
 // C library functions
 //======================================================================
 
-static void define_library(void *root, Obj **env) { }
-
-//======================================================================
-// Entry point
-//======================================================================
-
-int main(int argc, char *argv[]) {
+// (tigr)
+static Obj *prim_tigr(void *root, Obj **env, Obj **list) {
     Tigr *screen = tigrWindow(320, 240, "minigfx", 0);
     while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
@@ -19,5 +14,37 @@ int main(int argc, char *argv[]) {
         tigrUpdate(screen);
     }
     tigrFree(screen);
-    return 0;
+    return Nil;
+}
+
+static void define_library(void *root, Obj **env) {
+    add_primitive(root, env, "tigr", prim_tigr);
+}
+
+//======================================================================
+// Entry point
+//======================================================================
+
+int main(int argc, char *argv[]) {
+    // Memory allocation
+    memory = alloc_semispace();
+
+    // Constants, primitives and library
+    Symbols = Nil;
+    void *root = NULL;
+    DEFINE1(env);
+    *env = make_env(root, &Nil, &Nil);
+    define_constants(root, env);
+    define_primitives(root, env);
+    define_library(root, env);
+
+    // Mark a return point
+    if (setjmp(exception_env) != 0)
+        exit(1);
+
+    // Set up the reader
+    input = 1 < argc ? fopen(argv[1], "r") : stdin;
+
+    // The main loop
+    return repl(root, env, false);
 }
