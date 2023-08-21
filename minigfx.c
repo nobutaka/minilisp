b8921808b8fb7ade3ab6ea48a76c8b91983830f1
nobutaka <n.takushima@gmail.com>
2023-08-22 01:23:43 +0900

    format

--- minigfx.c
+++ minigfx.c
@@ -1,7 +1,7 @@
 #include "tigr.h"
 
-int main(int argc, char* argv[]) {
-    Tigr* screen = tigrWindow(320, 240, "Hello", 0);
+int main(int argc, char **argv) {
+    Tigr *screen = tigrWindow(320, 240, "Hello", 0);
     while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
         tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff), "Hello, world.");
