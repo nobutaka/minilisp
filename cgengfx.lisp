1a017dea5f2bf10f78288088071c1092a0c8e5a1
nobutaka <n.takushima@gmail.com>
2023-08-22 23:10:31 +0900

    add tfont tigrPrint

--- cgengfx.lisp
+++ cgengfx.lisp
@@ -1,13 +1,15 @@
 (load "cgen.lisp")
 
 (define decls '(
-  (pointer tigrWindow number number string number)  ; Tigr *tigrWindow(int w, int h, const char *title, int flags);
-  (void tigrFree pointer)                           ; void tigrFree(Tigr *bmp);
-  (number tigrClosed pointer)                       ; int tigrClosed(Tigr *bmp);
-  (void tigrUpdate pointer)                         ; void tigrUpdate(Tigr *bmp);
-  (void tigrClear pointer number)                   ; void tigrClear(Tigr *bmp, TPixel color);
-  (number tigrRGB number number number)             ; TPixel tigrRGB(unsigned char r, unsigned char g, unsigned char b);
-  (number tigrKeyDown pointer number)               ; int tigrKeyDown(Tigr *bmp, int key);
+  (pointer tigrWindow number number string number)              ; Tigr *tigrWindow(int w, int h, const char *title, int flags);
+  (void tigrFree pointer)                                       ; void tigrFree(Tigr *bmp);
+  (number tigrClosed pointer)                                   ; int tigrClosed(Tigr *bmp);
+  (void tigrUpdate pointer)                                     ; void tigrUpdate(Tigr *bmp);
+  (void tigrClear pointer number)                               ; void tigrClear(Tigr *bmp, TPixel color);
+  (number tigrRGB number number number)                         ; TPixel tigrRGB(unsigned char r, unsigned char g, unsigned char b);
+  (void tigrPrint pointer pointer number number number string)  ; void tigrPrint(Tigr *dest, TigrFont *font, int x, int y, TPixel color, const char *text, ...);
+  (pointer tfont)                                               ; TigrFont *tfont;
+  (number tigrKeyDown pointer number)                           ; int tigrKeyDown(Tigr *bmp, int key);
 ))
 
 (write-defs decls)
