70f433e31572a04093611f20bdc231a719ee22a9
nobutaka <n.takushima@gmail.com>
2023-08-22 23:01:59 +0900

    append C signatures

--- cgengfx.lisp
+++ cgengfx.lisp
@@ -1,13 +1,13 @@
 (load "cgen.lisp")
 
 (define decls '(
-  (pointer tigrWindow number number string number)
-  (void tigrFree pointer)
-  (number tigrClosed pointer)
-  (void tigrUpdate pointer)
-  (void tigrClear pointer number)
-  (number tigrRGB number number number)
-  (number tigrKeyDown pointer number)
+  (pointer tigrWindow number number string number)  ; Tigr *tigrWindow(int w, int h, const char *title, int flags);
+  (void tigrFree pointer)                           ; void tigrFree(Tigr *bmp);
+  (number tigrClosed pointer)                       ; int tigrClosed(Tigr *bmp);
+  (void tigrUpdate pointer)                         ; void tigrUpdate(Tigr *bmp);
+  (void tigrClear pointer number)                   ; void tigrClear(Tigr *bmp, TPixel color);
+  (number tigrRGB number number number)             ; TPixel tigrRGB(unsigned char r, unsigned char g, unsigned char b)
+  (number tigrKeyDown pointer number)               ; int tigrKeyDown(Tigr *bmp, int key);
 ))
 
 (write-defs decls)
