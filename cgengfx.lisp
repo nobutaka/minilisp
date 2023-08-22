f38e1358084a92eef92f1c7336f121ada8221fbb
nobutaka <n.takushima@gmail.com>
2023-08-22 22:35:58 +0900

    format & add more decls

--- cgengfx.lisp
+++ cgengfx.lisp
@@ -1,7 +1,11 @@
 (load "cgen.lisp")
 
-(define decls
-  '((pointer tigrWindow number number string number)
-    ))
+(define decls '(
+  (pointer tigrWindow number number string number)
+  (void tigrFree pointer)
+  (number tigrClosed pointer)
+  (void tigrUpdate pointer)
+  (number tigrKeyDown pointer number)
+))
 
 (write-defs decls)
--- cgensh.lisp
+++ cgensh.lisp
@@ -1,12 +1,13 @@
 (load "cgen.lisp")
 
-(define decls
-  '((pointer? fopen string string)
-    (number fclose pointer)
-    (number putchar number)
-    (void exit number)
-    (void free pointer?)
-    (number rand)
-    (number sin number)))
+(define decls '(
+  (pointer? fopen string string)
+  (number fclose pointer)
+  (number putchar number)
+  (void exit number)
+  (void free pointer?)
+  (number rand)
+  (number sin number)
+))
 
 (write-defs decls)
