7522786511ba0e4ab6de8e0ec56e1988b78d5c8f
nobutaka <n.takushima@gmail.com>
2023-08-22 22:55:52 +0900

    TPixel will be packed as number.

--- cgengfx.lisp
+++ cgengfx.lisp
@@ -5,6 +5,8 @@
   (void tigrFree pointer)
   (number tigrClosed pointer)
   (void tigrUpdate pointer)
+  (void tigrClear pointer number)
+  (number tigrRGB number number number)
   (number tigrKeyDown pointer number)
 ))
 
