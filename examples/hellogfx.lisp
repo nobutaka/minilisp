8f4540318743205e595b40580acb88bc7f86d79b
nobutaka <n.takushima@gmail.com>
2023-08-24 02:24:56 +0900

    pass dt

--- examples/hellogfx.lisp
+++ examples/hellogfx.lisp
@@ -2,7 +2,7 @@
 
 (define screen (tigrWindow 320 240 "Hello" 0))
 
-(defun update ()
+(defun update (dt)
   (tigrClear screen (tigrRGB 128 144 160))
   (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world."))
 
--- libgfx.lisp
+++ libgfx.lisp
@@ -7,5 +7,5 @@
   (tigrTime)
   (tigrUpdate screen)
   (while (and (if (= (tigrClosed screen) 0) t (exit 0)) (= (tigrKeyDown screen TK_ESCAPE) 0))
-    (update)
+    (update (tigrTime))
     (tigrUpdate screen)))
