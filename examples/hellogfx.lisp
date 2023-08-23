b551ad5256e19c7db6516f8cb42eab2000242cd4
nobutaka <n.takushima@gmail.com>
2023-08-24 02:03:36 +0900

    add lf

--- examples/hellogfx.lisp
+++ examples/hellogfx.lisp
@@ -1,6 +1,7 @@
 (load "libgfx.lisp")
 
 (define screen (tigrWindow 320 240 "Hello" 0))
+
 (defun update ()
   (tigrClear screen (tigrRGB 128 144 160))
   (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world."))
