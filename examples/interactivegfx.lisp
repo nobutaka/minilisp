e9c1be83c333e8637d4897333d0566ab2fa316f8
nobutaka <n.takushima@gmail.com>
2023-08-23 23:57:29 +0900

    continuable loop

--- examples/interactivegfx.lisp
+++ examples/interactivegfx.lisp
@@ -3,8 +3,13 @@
 (define TK_ESCAPE 164)
 
 (define screen (tigrWindow 320 240 "Hello" 0))
-(while (and (= (tigrClosed screen) 0) (= (tigrKeyDown screen TK_ESCAPE) 0))
-  (tigrClear screen (tigrRGB 128 144 160))
-  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world.")
-  (tigrUpdate screen))
-(tigrFree screen)
+(defun loop ()
+  (tigrUpdate screen)
+  (tigrTime)
+  (while (= (tigrKeyDown screen TK_ESCAPE) 0)
+    (if (not (= (tigrClosed screen) 0)) (exit 0))
+    (tigrClear screen (tigrRGB 128 144 160))
+    (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world.")
+    (tigrUpdate screen)))
+
+(loop)
