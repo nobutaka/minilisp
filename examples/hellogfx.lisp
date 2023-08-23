6078cdb4b8c8118d43eed72261da0c530b064d58
nobutaka <n.takushima@gmail.com>
2023-08-24 01:50:38 +0900

    interactive loop is default

--- examples/hellogfx.lisp
+++ examples/hellogfx.lisp
@@ -1,9 +1,8 @@
-(load "lib.lisp")
 (load "libgfx.lisp")
 
 (define screen (tigrWindow 320 240 "Hello" 0))
-(while (and (= (tigrClosed screen) 0) (= (tigrKeyDown screen TK_ESCAPE) 0))
+(defun update ()
   (tigrClear screen (tigrRGB 128 144 160))
-  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world.")
-  (tigrUpdate screen))
-(tigrFree screen)
+  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world."))
+
+(loop)
--- examples/interactivegfx.lisp
+++ examples/interactivegfx.lisp
@@ -1,14 +1,8 @@
-(load "lib.lisp")
 (load "libgfx.lisp")
 
 (define screen (tigrWindow 320 240 "Hello" 0))
-(defun loop ()
-  (tigrUpdate screen)
-  (tigrTime)
-  (while (= (tigrKeyDown screen TK_ESCAPE) 0)
-    (if (not (= (tigrClosed screen) 0)) (exit 0))
-    (tigrClear screen (tigrRGB 128 144 160))
-    (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world.")
-    (tigrUpdate screen)))
+(defun update ()
+  (tigrClear screen (tigrRGB 128 144 160))
+  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world."))
 
 (loop)
