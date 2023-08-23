254d2d87e7e1d8791ad37577d1de7193d84fa689
nobutaka <n.takushima@gmail.com>
2023-08-23 23:29:37 +0900

    cp hellogfx.lisp

---
+++ examples/interactivegfx.lisp
@@ -0,0 +1,10 @@
+(load "lib.lisp")
+(define tfont (tfont))
+(define TK_ESCAPE 164)
+
+(define screen (tigrWindow 320 240 "Hello" 0))
+(while (and (= (tigrClosed screen) 0) (= (tigrKeyDown screen TK_ESCAPE) 0))
+  (tigrClear screen (tigrRGB 128 144 160))
+  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world.")
+  (tigrUpdate screen))
+(tigrFree screen)
