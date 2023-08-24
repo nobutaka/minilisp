9e653623ca3078d4c5b82eecd25679f721d14c7b
nobutaka <n.takushima@gmail.com>
2023-08-24 11:30:47 +0900

    global dt

--- libgfx.lisp
+++ libgfx.lisp
@@ -3,13 +3,17 @@
 (define tfont (tfont))
 (define TK_ESCAPE 164)
 
+(define dt 0)
+
 (defun loop ()
   (tigrTime)
   (while (and (if (= (tigrClosed screen) 0) t (exit 0)) (= (tigrKeyDown screen TK_ESCAPE) 0))
-    (update (tigrTime))
+    (setq dt (tigrTime))
+    (update)
     (tigrUpdate screen))
   (tigrUpdate screen))
 
 (defun step ()
-  (update (/ 60))
+  (setq dt (/ 60))
+  (update)
   (tigrUpdate screen))
