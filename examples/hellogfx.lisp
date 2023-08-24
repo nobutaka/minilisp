08bcda297f785e615da7d82e6c49af33e6f23f95
nobutaka <n.takushima@gmail.com>
2023-08-24 11:58:33 +0900

    animation

--- examples/hellogfx.lisp
+++ examples/hellogfx.lisp
@@ -1,9 +1,11 @@
 (load "libgfx.lisp")
 
 (define screen (tigrWindow 320 240 "Hello" 0))
+(define x 0)
 
 (defun update ()
+  (setq x (+ x (* dt 16)))
   (tigrClear screen (tigrRGB 128 144 160))
-  (tigrPrint screen tfont 120 110 (tigrRGB 255 255 255) "Hello, world."))
+  (tigrPrint screen tfont x 110 (tigrRGB 255 255 255) "Hello, world."))
 
 (loop)
