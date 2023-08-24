c70f3f5ceb61ed8a71b691773b391f904b935f62
nobutaka <n.takushima@gmail.com>
2023-08-24 11:00:53 +0900

    post tigrUpdate pops ESCAPE key

--- libgfx.lisp
+++ libgfx.lisp
@@ -5,7 +5,7 @@
 
 (defun loop ()
   (tigrTime)
-  (tigrUpdate screen)
   (while (and (if (= (tigrClosed screen) 0) t (exit 0)) (= (tigrKeyDown screen TK_ESCAPE) 0))
     (update (tigrTime))
-    (tigrUpdate screen)))
+    (tigrUpdate screen))
+  (tigrUpdate screen))
